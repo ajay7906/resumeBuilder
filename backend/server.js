9304b377deff020ade1aeb5509a7470c06fd5073
Ajay Kumar <aks969014@gmail.com>
2025-04-03 00:11:36 +0530

    add frontend

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,9 @@
+const express = require('express');
+
 const app = express();
+//defined body parser and cors
+const bodyParser = require('body-parser');
+const cors = require('cors');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
